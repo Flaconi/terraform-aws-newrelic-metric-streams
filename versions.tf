a864f7e69a289970a90dbdc37ab99a8172e451be
Alexander Miehe <alexander.miehe@flaconi.de>
2023-06-09 11:13:18 +0300

    PLT-0 - Init

--- versions.tf
+++ versions.tf
@@ -1,3 +1,14 @@
 terraform {
-  required_version = "~> 1.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.53"
+    }
+
+    newrelic = {
+      source  = "newrelic/newrelic"
+      version = "~> 3.22.0"
+    }
+  }
+  required_version = ">= 1.3"
 }
